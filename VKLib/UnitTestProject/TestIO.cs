1688361300cc6d84154634f7b576b4eba3385e10
jooho <ravenerkr@gmail.com>
2019-04-18 08:59:32 +0900

    폴더 관련 기능 추가.

---
+++ VKLib/UnitTestProject/TestIO.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using VKLib.IO;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class TestIO
+    {
+        [Test]
+        public void Test_RemoveNotUseableCharsToFolderName()
+        {
+            var folderName2 = "aa:aa";
+            Assert.AreEqual("aa aa", FolderNameFixer.Fix(folderName2));
+        }
+    }
+}
---
+++ VKLib/VKLib/NativeExtension/IDictionaryExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace VKLib.NativeExtension
+{
+    public static class IDictionaryExtension
+    {
+        public static void Put<Tkey, Tvalue>(this IDictionary<Tkey, Tvalue> dic, Tkey key, Tvalue value)
+        {
+            if (dic.ContainsKey(key))
+            {
+                dic[key] = value;
+            }
+            else
+            {
+                dic.Add(key, value);
+            }
+        }
+    }
+}
