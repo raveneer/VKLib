df6760d39f1db190b9ca2d5d67d7e742675664ce
jooho <ravenerkr@gmail.com>
2020-10-09 17:45:02 +0900

    네임스페이스 세팅

--- VKLib/Network/AdManager.cs
+++ VKLib/Network/AdManager.cs
@@ -5,104 +5,107 @@ using UnityEngine.SceneManagement;
 using VKLib.Native;
 using Zenject;
 
-public class AdManager : MonoBehaviour
+namespace VKLib.Network
 {
-    [Inject] private EventManager _eventManager;
-    [Inject] private IADPolicy _iadPolicy;
-    private Action _adFailedAction;
-    private Action _adFinishAction;
-    private Action _adSkippedActon;
-
-    private void Start()
+    public class AdManager : MonoBehaviour
     {
-        Initialize();
-        _eventManager.SceneLoadAfterAdShow += OnSceneLoadAfterAdShow;
-    }
+        [Inject] private EventManager _eventManager;
+        [Inject] private IADPolicy _iadPolicy;
+        private Action _adFailedAction;
+        private Action _adFinishAction;
+        private Action _adSkippedActon;
 
-    private void Initialize()
-    {
-    #if UNITY_ANDROID
-        Advertisement.Initialize(_iadPolicy.GoogleGameID);
-    #elif UNITY_IOS
+        private void Start()
+        {
+            Initialize();
+            _eventManager.SceneLoadAfterAdShow += OnSceneLoadAfterAdShow;
+        }
+
+        private void Initialize()
+        {
+        #if UNITY_ANDROID
+            Advertisement.Initialize(_iadPolicy.GoogleGameID);
+        #elif UNITY_IOS
         Advertisement.Initialize(_iadPolicy.AppleGameID);
-    #endif
-    }
-
-    private void OnSceneLoadAfterAdShow()
-    {
-        if (_iadPolicy.IsConditionMet())
-        {
-            ShowAd(ReloadScene, ReloadScene, ReloadScene);
+        #endif
         }
-        else
+
+        private void OnSceneLoadAfterAdShow()
         {
-            Debug.LogWarning("Ad condition not met, cancel Ad show");
-            ReloadScene();
-        }
-    }
-
-    private void HandleShowResult(ShowResult result)
-    {
-        switch (result)
-        {
-            case ShowResult.Finished:
+            if (_iadPolicy.IsConditionMet())
             {
-                Debug.Log("The ad was successfully shown.");
-
-                // to do ...
-                // 광고 시청이 완료되었을 때 처리
-                _adFinishAction?.Invoke();
-
-                break;
+                ShowAd(ReloadScene, ReloadScene, ReloadScene);
             }
-            case ShowResult.Skipped:
+            else
             {
-                Debug.Log("The ad was skipped before reaching the end.");
-
-                // to do ...
-                // 광고가 스킵되었을 때 처리
-                _adSkippedActon?.Invoke();
-                break;
-            }
-            case ShowResult.Failed:
-            {
-                Debug.LogError("The ad failed to be shown.");
-
-                // to do ...
-                // 광고 시청에 실패했을 때 처리
-                _adFailedAction?.Invoke();
-                break;
+                Debug.LogWarning("Ad condition not met, cancel Ad show");
+                ReloadScene();
             }
         }
-    }
 
-    //todo : 책임소재가 틀렸음. 다른데로 보낼 것.
-    private void ReloadScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    private void ShowAd(Action adFinished, Action adSkipped, Action adFailed)
-    {
-        _adFinishAction = adFinished;
-        _adSkippedActon = adSkipped;
-        _adFailedAction = adFailed;
-
-        if (Advertisement.IsReady(_iadPolicy.RewardVideoID))
+        private void HandleShowResult(ShowResult result)
         {
-            var options = new ShowOptions {resultCallback = HandleShowResult};
+            switch (result)
+            {
+                case ShowResult.Finished:
+                {
+                    Debug.Log("The ad was successfully shown.");
 
-            Advertisement.Show(_iadPolicy.RewardVideoID, options);
+                    // to do ...
+                    // 광고 시청이 완료되었을 때 처리
+                    _adFinishAction?.Invoke();
+
+                    break;
+                }
+                case ShowResult.Skipped:
+                {
+                    Debug.Log("The ad was skipped before reaching the end.");
+
+                    // to do ...
+                    // 광고가 스킵되었을 때 처리
+                    _adSkippedActon?.Invoke();
+                    break;
+                }
+                case ShowResult.Failed:
+                {
+                    Debug.LogError("The ad failed to be shown.");
+
+                    // to do ...
+                    // 광고 시청에 실패했을 때 처리
+                    _adFailedAction?.Invoke();
+                    break;
+                }
+            }
         }
+
+        //todo : 책임소재가 틀렸음. 다른데로 보낼 것.
+        private void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        private void ShowAd(Action adFinished, Action adSkipped, Action adFailed)
+        {
+            _adFinishAction = adFinished;
+            _adSkippedActon = adSkipped;
+            _adFailedAction = adFailed;
+
+            if (Advertisement.IsReady(_iadPolicy.RewardVideoID))
+            {
+                var options = new ShowOptions {resultCallback = HandleShowResult};
+
+                Advertisement.Show(_iadPolicy.RewardVideoID, options);
+            }
+        }
+
+
     }
 
-
-}
-
-public interface IADPolicy
-{
-    bool IsConditionMet();
-    string RewardVideoID { get; }
-    string GoogleGameID { get; }
-    string AppleGameID { get; }
+    public interface IADPolicy
+    {
+        bool IsConditionMet();
+        string RewardVideoID { get; }
+        string GoogleGameID { get; }
+        string AppleGameID { get; }
+    }
 }
