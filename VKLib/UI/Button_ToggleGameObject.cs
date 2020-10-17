70094f64e77ac1bbfe2768d31c71de82e2b50f1c
jooho <ravenerkr@gmail.com>
2020-10-17 20:09:47 +0900

    다른 게임오브젝트를 단순히 켜고 끄는 버튼의 추가.

---
+++ VKLib/UI/Button_ToggleGameObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using VKLib.Native;
+
+namespace VKLib.UI
+{
+    public class Button_ToggleGameObject : MonoBehaviour
+    {
+        public GameObject TargetGo;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            TDebug.NoNull(TargetGo);
+            GetComponent<Button>().onClick.AddListener(() => TargetGo.SetActive(!TargetGo.activeSelf));
+        }
+
+    }
+}
