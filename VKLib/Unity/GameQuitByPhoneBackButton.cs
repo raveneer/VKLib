f3caca82bcbfc34882345f1ebf41727977acecb0
jooho <ravenerkr@gmail.com>
2020-10-18 00:45:41 +0900

    뒤로가기 버튼으로 앱을 끄는 기능의 추가. 자이로 컨트롤러 추가.

---
+++ VKLib/Unity/GameQuitByPhoneBackButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace VKLib.Unity
+{
+    /// <summary>
+    ///     폰의 뒤로가기 버튼으로 닫기.
+    ///     현재 안드폰만 지원중
+    /// </summary>
+    public class GameQuitByPhoneBackButton : MonoBehaviour
+    {
+        private void Update()
+        {
+            // 안드로이드 백키가 눌렸을때
+            if (Application.platform == RuntimePlatform.Android && Input.GetKeyDown(KeyCode.Escape))
+            {
+                Quit();
+            }
+        }
+
+        private void Quit()
+        {
+            try
+            {
+                Application.Quit();
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
---
+++ VKLib/Unity/GyroControll.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace VKLib.Unity
+{
+    /// <summary>
+    /// https://www.youtube.com/watch?v=P5JxTfCAOXo
+    /// 폰의 자이로에 따라 회전한다. 왜 컨테이너에 넣어주는지는 아직 잘 모르겠음...
+    /// </summary>
+    public class GyroControll : MonoBehaviour
+    {
+        private bool _gyroEnabled;
+        private Gyroscope _gyro;
+        private GameObject _container;
+        private Quaternion _rot;
+
+        private void Start()
+        {
+            _container = new GameObject("Gyro Container");
+            _container.transform.position = transform.position;
+            transform.SetParent(_container.transform);
+            _gyroEnabled = EnableGyro();
+
+            Debug.Log($"system gyro support : {SystemInfo.supportsGyroscope}");
+        }
+
+        private bool EnableGyro()
+        {
+            if (SystemInfo.supportsGyroscope)
+            {
+                _gyro = Input.gyro;
+                _gyro.enabled = true;
+
+                _container.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+                _rot = new Quaternion(0, 0, 1, 0);
+                return true;
+            }
+            return false;
+        }
+
+        private void Update()
+        {
+            if (_gyroEnabled)
+            {
+                transform.localRotation = _gyro.attitude * _rot;
+            }
+        }
+    }
+}
