24c7979029a092c99c8f70bcfbd98dd706b82bbf
jooho <ravenerkr@gmail.com>
2020-11-26 17:01:04 +0900

    값과 아이템을 주면 빈도로 뽑아주는 클래스 추가.

---
+++ VKLib/Native/Frequency.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VKLib.Native
+{
+    public class Frequency<T>
+    {
+        private readonly List<(float, T)> _frequents = new List<(float, T)>();
+        private float _maxPercent;
+
+        public Frequency(params (float, T)[] elems)
+        {
+            var threshold = 0f;
+            foreach (var elem in elems)
+            {
+                threshold += elem.Item1;
+                _frequents.Add((threshold, elem.Item2));
+            }
+            _maxPercent = threshold;
+        }
+
+        public T GetNextItem()
+        {
+            TDebug.Assert(_frequents.Any());
+            var randomPercent = TRandom.Range(0f, _maxPercent);
+
+            for (int i = 0; i < _frequents.Count; i++)
+            {
+                if (randomPercent <= _frequents[i].Item1)
+                {
+                    return _frequents[i].Item2;
+                }
+            }
+            return default(T);
+        }
+    }
+}
