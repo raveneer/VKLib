5e80a19c6931723f7028acf07115e740da35fe1d
jooho <ravenerkr@gmail.com>
2020-12-29 17:14:30 +0900

    스프라이트의 꼭짓점들의 월드좌표를 반환하는 헬퍼함수 추가

---
+++ VKLib/UnityExtension/SpriteHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.PDC.Extensions
+{
+    public static class SpriteHelper
+    {
+        /// <summary>
+        /// https://answers.unity.com/questions/641006/is-there-a-way-to-get-a-sprites-current-widthheigh.html
+        /// 스프라이트의 엣지의 월드 좌표를 받아냄.
+        /// </summary>
+        public static Vector3[] SpriteLocalToWorld(Transform transform, Sprite sp) 
+        {
+            Vector3 pos = transform.position;
+            Vector3 [] array = new Vector3[2];
+            //top left
+            array[0] = pos + sp.bounds.min;
+            // Bottom right
+            array[1] = pos + sp.bounds.max;
+            return array;
+        }
+    }
+}
