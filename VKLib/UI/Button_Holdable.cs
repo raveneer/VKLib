b505e00c997ba22dfb90320807514110e4599205
jooho <ravenerkr@gmail.com>
2021-01-04 11:52:38 +0900

    빠진 스크립 추가 홀드버튼

---
+++ VKLib/UI/Button_Holdable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VKLib.Native;
+
+namespace VKLib.VKLib.UI
+{
+    public class Button_Holdable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public Action FireEvent;
+
+        public UnityEvent FireUnityEvent;
+        private float contiueRemainSec;
+
+        /// <summary>
+        ///     홀드 상태가 되면, 이 시간마다 이벤트를 쏜다.
+        /// </summary>
+        private readonly float contiueTermSec = 0.3f;
+
+        [SerializeField]
+        private Image fillImage;
+        private bool pointerDown;
+        private float pointerDownTimer;
+
+        [SerializeField]
+        private float requiredHoldTime = 1f;
+
+        private void Start()
+        {
+            Reset();
+            FireEvent += () => FireUnityEvent?.Invoke();
+        }
+
+        /// <summary>
+        ///     일정시간 이상 누르고 있으면 일정 텀으로 연사함.
+        /// </summary>
+        private void Update()
+        {
+            if (pointerDown)
+            {
+                fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+                pointerDownTimer += Time.deltaTime;
+                if (pointerDownTimer >= requiredHoldTime)
+                {
+                    contiueRemainSec += Time.deltaTime;
+                    if (contiueRemainSec >= contiueTermSec)
+                    {
+                        contiueRemainSec -= contiueTermSec;
+                        FireEvent?.Invoke();
+                        TDebug.Log("LongLongClicks fire!");
+                    }
+                }
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            pointerDown = true;
+            Debug.Log("OnPointerDown");
+        }
+
+        /// <summary>
+        ///     뗄떼도 한번은 발동 해 주어야 함
+        /// </summary>
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            FireEvent?.Invoke();
+
+            Reset();
+            Debug.Log("OnPointerUp");
+        }
+
+        private void Reset()
+        {
+            pointerDown = false;
+            pointerDownTimer = 0;
+            fillImage.fillAmount = 0;
+        }
+    }
+}
