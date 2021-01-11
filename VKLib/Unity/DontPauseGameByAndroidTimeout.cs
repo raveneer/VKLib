ce723ae0d6d1be59fb7c2b2f5f31dc71c514a40e
jooho <ravenerkr@gmail.com>
2021-01-11 13:42:12 +0900

    게임 자동으로 꺼지지 않게

---
+++ VKLib/Unity/DontPauseGameByAndroidTimeout.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VKLib.Unity
+{
+    /// <summary>
+    ///    게임을 켜두면 폰이 꺼지지 않게 됨.
+    /// </summary>
+    public class DontPauseGameByAndroidTimeout : MonoBehaviour
+    {
+        private void Start()
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+    }
+}
