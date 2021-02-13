0d51726306a6d6c706a9a1467411b6f0fae5bf95
jooho <ravenerkr@gmail.com>
2021-02-13 19:02:31 +0900

    아날리틱스는 에디터에서 안 보내게 됨

--- VKLib/Network/UnityAnalyticsManager.cs
+++ VKLib/Network/UnityAnalyticsManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 using VKLib.Native;
 using Zenject;
 using UnityEngine.Analytics;
 
 namespace VKLib.Network
 {
+    /// <summary>
+    /// todo : 개발자 계정이나 에디터에서는 보내지 말아야 (집계되면 안되니까) 할 것. 일단은 에디터 막기.
+    /// </summary>
     public class UnityAnalyticsManager : IInitializable
     {
         [Inject] private EventManager _eventManager;
@@ -15,6 +19,11 @@ namespace VKLib.Network
 
         private void OnSendUnityAnalyticsCustomEvent(string eventName, string key, object value)
         {
+            if (Application.isEditor)
+            {
+                TDebug.Log($"{eventName} {key}  {value.ToString()} 아날리틱스 발동하였으나, 에디터 이므로 패스.");
+            }
+
             var res = Analytics.CustomEvent(eventName, new Dictionary<string, object>() {{key, value}});
             if (res == AnalyticsResult.Ok)
             {
