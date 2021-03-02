c3734157cd7bb292b70a3d38b0fce5616c945362
joohokim <ravenerkr@gmail.com>
2021-03-02 23:38:15 +0900

    잔상효과 이펙트 추가

---
+++ VKLib/Unity/DelaydeSpriteEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelaydeSpriteEffect : MonoBehaviour
+{
+    public SpriteRenderer OriginSpriteRenderer;
+    public SpriteRenderer Renderer;
+    private Sprite _cachedSprite;
+    public int DelayFrame = 2;
+
+    private Queue<Sprite> delaySpritesQueue = new Queue<Sprite>();
+
+    private void Update()
+    {
+        if (delaySpritesQueue.Count < DelayFrame)
+        {
+            delaySpritesQueue.Enqueue(OriginSpriteRenderer.sprite);
+        }
+        else
+        {
+            var sp = delaySpritesQueue.Dequeue();
+            Renderer.sprite = sp;
+            delaySpritesQueue.Enqueue(OriginSpriteRenderer.sprite);
+        }
+
+    }
+    
+    
+}
