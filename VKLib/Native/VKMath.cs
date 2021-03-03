9b131b9314b7f630a03046ab1d75dae819a6e12d
jooho <ravenerkr@gmail.com>
2021-03-03 17:45:39 +0900

    VKMath의 추가. Progress 와 Lerp의 추가.

---
+++ VKLib/Native/VKMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VKLib.Native
+{
+    public static class VKMath
+    {
+        public static float Lerp(float start, float end, float t)
+        {
+            return start + t * (end - start); }
+
+        public static float Progress(float current, float currentLvStart, float nextLvStart)
+        {
+            if (nextLvStart == currentLvStart)
+            {
+                throw new Exception($"nextLvStart must larger than currentLvStart! n:{nextLvStart}, c:{currentLvStart}");
+            }
+
+            return (current - currentLvStart) / (nextLvStart - currentLvStart);
+        }
+    }
+
+}
