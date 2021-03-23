130189769a312a7db8b783406b7a22e8bb07070a
joohokim <ravenerkr@gmail.com>
2021-03-23 09:41:46 +0900

    URL여는 버튼 추가

---
+++ VKLib/Unity/Button_OpenURL.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace VKLib.Unity
+{
+    public class Button_OpenURL : MonoBehaviour
+    {
+        public string URL;
+        public Button Button;
+        void Awake()
+        {
+            Button.onClick.AddListener(()=>Application.OpenURL(URL));
+        }
+    }    
+}
+
