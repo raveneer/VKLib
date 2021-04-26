bda034b009ff303e68f20178994d7c27f3b3f68a
joohokim <ravenerkr@gmail.com>
2021-04-26 14:55:59 +0900

    유니티 아날리틱스를 큐로 보냄. 틱 부하 줄임

--- VKLib/Network/UnityAnalyticsManager.cs
+++ VKLib/Network/UnityAnalyticsManager.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Analytics;
 using VKLib.Native;
 using Zenject;
-using UnityEngine.Analytics;
 
 namespace VKLib.Network
 {
     /// <summary>
-    /// todo : 개발자 계정이나 에디터에서는 보내지 말아야 (집계되면 안되니까) 할 것. 일단은 에디터 막기.
+    ///     todo : 개발자 계정이나 에디터에서는 보내지 말아야 (집계되면 안되니까) 할 것. 일단은 에디터 막기.
+    ///     처리량이 많으면 게임이 느려지곤 하기 때문에, 큐잉으로 처리한다. 1틱에 1건씩을 보고한다.
+    ///     이걸로도 느리면, 스레드를 써야 할 것이다...
     /// </summary>
     public class UnityAnalyticsManager : IInitializable
     {
         [Inject] private EventManager _eventManager;
+        private readonly Queue<(string eventName, string key, object value)> _sendQueue = new Queue<(string, string, object)>();
+
         public void Initialize()
         {
             _eventManager.SendUnityAnalyticsCustomEvent += OnSendUnityAnalyticsCustomEvent;
+            _eventManager.Tick += OnTick;
+        }
+
+        private void OnTick(float delta)
+        {
+            if (_sendQueue.Any())
+            {
+                SendAnalytics();
+            }
         }
 
         private void OnSendUnityAnalyticsCustomEvent(string eventName, string key, object value)
@@ -24,7 +38,18 @@ namespace VKLib.Network
                 TDebug.Log($"{eventName} {key}  {value.ToString()} 아날리틱스 발동하였으나, 에디터 이므로 패스.");
                 return;
             }
+            else
+            {
+                _sendQueue.Enqueue((eventName, key, value));
+            }
+        }
 
+        private void SendAnalytics()
+        {
+            var elem = _sendQueue.Dequeue();
+            var eventName = elem.eventName;
+            var key = elem.key;
+            var value = elem.value;
             var res = Analytics.CustomEvent(eventName, new Dictionary<string, object>() {{key, value}});
             if (res == AnalyticsResult.Ok)
             {
@@ -34,7 +59,6 @@ namespace VKLib.Network
             {
                 TDebug.LogWarning($"unity analytics sent fail! code {res} : {eventName}, {key}, {value.ToString()}");
             }
-        
         }
     }
 }
