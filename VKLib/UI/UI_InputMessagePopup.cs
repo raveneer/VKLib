3bd27173f9f2cca2140fa559ac3bb89e62969ca5
joohokim <ravenerkr@gmail.com>
2021-04-30 12:03:08 +0900

    인풋 팝업을 라이브러리로 빼냄! 잘했다.

---
+++ VKLib/UI/UI_InputMessagePopup.cs
@@ -0,0 +1,53 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VKLib.Native;
+using Zenject;
+
+namespace VKLib.UI
+{
+    /// <summary>
+    /// 유저에게 입력을 받는다. 취소는 불가능함. (단순하게, 단순하게 만듭시다)
+    /// </summary>
+    public class UI_InputMessagePopup :MonoBehaviour
+    {
+        [Inject] private readonly EventManager _eventManager;
+        public InputField InputField;
+        public TextMeshProUGUI TMP_Message;
+        public Button Button_Confirm;
+        public Button Button_Close;
+        public GameObject Panel;
+        public RectTransform InputFieldRect;
+
+        private void Awake()
+        {
+            _eventManager.InputMessagePopup += OnInputMessagePopup;
+            Button_Close.onClick.AddListener(()=>Panel.gameObject.SetActive(false));
+            Panel.gameObject.SetActive(false);
+        }
+
+        private void OnInputMessagePopup(string message, Action<string> action, bool isLargeInputField)
+        {
+            Panel.gameObject.SetActive(true);
+            TMP_Message.text = message;
+
+            Button_Confirm.onClick.RemoveAllListeners();
+            Button_Confirm.onClick.AddListener(()=>
+            {
+                Panel.gameObject.SetActive(false);
+                //액션의 순서에 주의. 이렇게 꺼준 후 해줘야 inputpopup이 또  inputpopup 부를때 연결이 됨.
+                action(InputField.text);
+            });
+
+            if (isLargeInputField)
+            {
+                InputFieldRect.sizeDelta = new Vector2(InputFieldRect.sizeDelta.x, 150);
+            }
+            else
+            {
+                InputFieldRect.sizeDelta = new Vector2(InputFieldRect.sizeDelta.x, 50);
+            }
+        }
+    }
+}
