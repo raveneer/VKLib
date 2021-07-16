f4f6b56073d254a0083fdae54bd7a248655ec38b
joohokim <ravenerkr@gmail.com>
2021-07-16 22:58:49 +0900

    오토빌더를 VKLib으로 옮김

--- VKLib/UI/UI_VersionText.cs
+++ VKLib/UI/UI_VersionText.cs
@@ -3,14 +3,9 @@ using UnityEngine;
 
 public class UI_VersionText : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
         var tmp = GetComponent<TextMeshProUGUI>();
         tmp.text = $"ver:{Application.version}";
-    }
-
-    void Update()
-    {
-        
     }
 }
